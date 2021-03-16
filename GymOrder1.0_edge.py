b3a03455c9f7db8c8ff58cd588b5b9ed766cb787
yindaheng98 <yindaheng98@163.com>
2021-03-16 18:16:36 +0800

    能打开预定界面了

--- GymOrder1.0_edge.py
+++ GymOrder1.0_edge.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 import random
 import json
+import datetime
 
 # 加启动配置 禁用日志log
 # ie capabilities
@@ -116,7 +117,7 @@ def enterOrderList():
 
     print('预约如下时间的场馆')
     for order in order_list:
-        date,time = order
+        date, time = order
         print('预约%s %s的场馆' % (date_list[str(date)], time_list[str(time)]))
 
     return order_list
@@ -151,81 +152,48 @@ def check(text, browser):
     return False
 
 
-def main(browser):
-    pass
+def make_order(date, t):
+    date, t = str(date), str(t)
+    try:
+        print("预约一个%s %s的场馆" % (date_list[date], time_list[t]))
+        today = datetime.date.today()
+        for i in range(0, 3):
+            day = today + datetime.timedelta(days=i)
+            weekday = str(day.weekday()+1)
+            if weekday == date:
+                browser = webdriver.Edge(executable_path='./msedgedriver.exe')
+                print("------------------浏览器已启动----------------------")
+                login(user, pw, browser)
+                browser.implicitly_wait(5)
+                time.sleep(5)
+                str_day = day.strftime('%Y-%m-%d')
+                str_weekday = date_list[weekday]
+                str_time = time_list[t]
+                print("%s是%s, 可以预约" % (str_day, str_weekday))
+                browser.execute_script("changeInfo(null, '10', '羽毛球（九龙湖）')")
+                browser.implicitly_wait(5)
+                time.sleep(5)
+                browser.execute_script(
+                    "changeInfo('"+str_day+str_weekday+"', 0, null)")
+                browser.implicitly_wait(5)
+                time.sleep(5)
+                browser.execute_script("orderSite('"+str_time+"')")
+                browser.implicitly_wait(5)
+                time.sleep(5)
+                browser.close()
+                print("------------------浏览器已关闭----------------------")
+                time.sleep(3)
+                break
+    except Exception as r:
+        print("未知错误 %s" % (r))
+
+
+def make_orders(order_list):
+    for order in order_list:
+        make_order(order[0], order[1])
 
 
 if __name__ == "__main__":
     user, pw = enterUserPW()
     order_list = enterOrderList()
-    localtime = time.localtime(time.time())
-    set_minite = localtime.tm_min  # 首次登陆的分钟时刻，代表以后每次在此分钟时刻打卡
-    set_hour = localtime.tm_hour  # 首次登陆的时钟时刻，代表以后每次在此时钟时刻打卡
-
-    if set_hour > 9:
-        set_hour = 7  # 如果首次登录超过上午10点，则以后默认在7点钟打卡
-        first_time = True
-
-    while True:
-        try:
-            # 登录打卡一次试一试
-            browser = webdriver.Edge(executable_path='./msedgedriver.exe')
-            print("------------------浏览器已启动----------------------")
-            login(user, pw, browser)
-            browser.implicitly_wait(10)
-            time.sleep(10)
-
-            # 确认是否打卡成功
-            # 的确无新增按钮
-            dailyDone = not check("新增", browser)
-            if dailyDone is True and check("退出", browser) is True:  # 今日已完成打卡
-                sleep_time = (set_hour+24-time.localtime(time.time()).tm_hour) * \
-                    3600 + (set_minite-time.localtime(time.time()).tm_min)*60
-                writeLog("下次打卡时间：明天" + str(set_hour) + ':' +
-                         str(set_minite) + "，" + "即" + str(sleep_time) + 's后')
-                browser.close()
-                print("------------------浏览器已关闭----------------------")
-                time.sleep(sleep_time)
-            elif dailyDone is False:  # 今日未完成打卡
-                # 点击报平安
-                buttons = browser.find_elements_by_css_selector('button')
-                for button in buttons:
-                    if button.get_attribute("textContent").find("新增") >= 0:
-                        button.click()
-                        browser.implicitly_wait(10)
-
-                        # 输入温度37°
-                        inputfileds = browser.find_elements_by_tag_name(
-                            'input')
-                        for i in inputfileds:
-                            if i.get_attribute("placeholder").find("请输入当天晨检体温") >= 0:
-                                i.click()
-                                i.send_keys(str(random.randint(365, 370)/10.0))
-
-                                # 确认并提交
-                                buttons = browser.find_elements_by_tag_name(
-                                    'button')
-                                for button in buttons:
-                                    if button.get_attribute("textContent").find("确认并提交") >= 0:
-                                        button.click()
-                                        buttons = browser.find_elements_by_tag_name(
-                                            'button')
-                                        button = buttons[-1]
-                                        # 提交
-                                        if button.get_attribute("textContent").find("确定") >= 0:
-                                            button.click()
-                                            dailyDone = True  # 标记已完成打卡
-                                            writeLog("打卡成功")
-                                        break
-                                break
-                        break
-                browser.close()
-                print("------------------浏览器已关闭----------------------")
-                time.sleep(10)  # 昏睡10s 为了防止网络故障未打上卡
-            else:
-                browser.close()
-                print("------------------网站出现故障----------------------")
-                print("------------------浏览器已关闭----------------------")
-                time.sleep(300)  # 昏睡5min 为了防止网络故障未打上卡
-        except Exception as r:
-            print("未知错误 %s" % (r))
+    make_orders(order_list)

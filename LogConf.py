6c5d50e11396d2c7a9834ae5d1dc6b3e92c43ecc
yindaheng98 <yindaheng98@163.com>
2021-04-06 12:42:06 +0800

    bug fixed

--- LogConf.py
+++ LogConf.py
@@ -5,7 +5,7 @@ import sys
 from logging import handlers
 
 def getLogger():
-    fname = 'logs/%s.' % os.path.basename(sys.argv[0])
+    fname = 'logs/%s.log' % os.path.basename(sys.argv[0])
     logger = logging.getLogger(fname)
     th = handlers.TimedRotatingFileHandler(filename=fname, when='MIDNIGHT', backupCount=3, encoding='utf-8')
     th.setFormatter(logging.Formatter('%(asctime)s\t%(levelname)s\t%(filename)s:%(lineno)d\t%(message)s', '%I:%M:%S %p'))

3f3cba8b0b21de4ec8fbef8c2ce5038fae555620
yindaheng98 <yindaheng98@163.com>
2021-09-30 20:52:06 +0800

    由于cookie的时效性，SEURobot需要在运行时动态生成

--- SEURobot_config.py
+++ SEURobot_config.py
@@ -2,9 +2,14 @@ import time
 
 from SEURobot import SEURobot
 from login_config import username, password
-seu_bot = SEURobot(username, password)
+
+
+def get_seubot():
+    return SEURobot(username, password)
+
 
 if __name__ == "__main__":
+    seu_bot = get_seubot()
     browser = seu_bot.open(
         "http://yuyue.seu.edu.cn/eduplus/order/fetchMyOrders.do?sclId=1")
     browser.implicitly_wait(10)

706d82e80e0d1d3df9902bd3c88de2d2d542bd10
yindaheng98 <yindaheng98@163.com>
2021-10-05 21:12:07 +0800

    docker部署

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9
+RUN pip install Pillow==8.2.0 requests==2.25.1 selenium==3.141.0
+COPY *.py /
+COPY Models /Models
+WORKDIR /
+RUN mkdir /logs
+CMD python GymOrder.py
--- driver_config.py
+++ driver_config.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.set_capability('browserless.token', 'YOUR-API-TOKEN')
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+
 
 def webdriver_init():
-    return webdriver.Edge(executable_path='./msedgedriver.exe')
+    return webdriver.Remote(
+        command_executor='http://browserless:3000/webdriver',
+        desired_capabilities=chrome_options.to_capabilities()
+    )  # webdriver.Edge(executable_path='./msedgedriver.exe')
